86e08543bb596a1b04f97c5f2be2c9f0b9fc8115
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-19 14:16:32 +0200

    Football class megírása

--- src/index.js
+++ src/index.js
@@ -1,1 +1,43 @@
 "use strict";
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (receiver, state, kind, f) {
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a getter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
+    return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
+};
+var _Football_loser, _Football_eredmeny, _Football_csapat1goalnum, _Football_csapat2goalnum;
+function randomResultNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+class Football {
+    constructor(csapat1, csapat2, date) {
+        _Football_loser.set(this, void 0);
+        _Football_eredmeny.set(this, void 0);
+        _Football_csapat1goalnum.set(this, void 0);
+        _Football_csapat2goalnum.set(this, void 0);
+        this.date = date;
+        __classPrivateFieldSet(this, _Football_csapat1goalnum, randomResultNumber(0, 6), "f");
+        __classPrivateFieldSet(this, _Football_csapat2goalnum, randomResultNumber(0, 6), "f");
+        if (__classPrivateFieldGet(this, _Football_csapat1goalnum, "f") > __classPrivateFieldGet(this, _Football_csapat2goalnum, "f")) {
+            __classPrivateFieldSet(this, _Football_eredmeny, __classPrivateFieldGet(this, _Football_csapat1goalnum, "f") + "-" + __classPrivateFieldGet(this, _Football_csapat2goalnum, "f"), "f");
+            this.winner = csapat1;
+            __classPrivateFieldSet(this, _Football_loser, csapat2, "f");
+        }
+        else {
+            __classPrivateFieldSet(this, _Football_eredmeny, __classPrivateFieldGet(this, _Football_csapat2goalnum, "f") + "-" + __classPrivateFieldGet(this, _Football_csapat1goalnum, "f"), "f");
+            this.winner = csapat2;
+            __classPrivateFieldSet(this, _Football_loser, csapat1, "f");
+        }
+    }
+    result() {
+        return "Football match: " + this.winner + " - " + __classPrivateFieldGet(this, _Football_loser, "f") + " (" + __classPrivateFieldGet(this, _Football_eredmeny, "f") + ")";
+    }
+}
+_Football_loser = new WeakMap(), _Football_eredmeny = new WeakMap(), _Football_csapat1goalnum = new WeakMap(), _Football_csapat2goalnum = new WeakMap();
+class Marathon {
+}
--- src/index.ts
+++ src/index.ts
@@ -4,4 +4,33 @@ interface Results {
     result(): string;
 }
 
+function randomResultNumber(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
+class Football implements Results {
+    winner: string;
+    date: Date;
+    #loser: string;
+    #eredmeny: string;
+    #csapat1goalnum: number;
+    #csapat2goalnum: number;
+
+    constructor(csapat1: string, csapat2: string, date: Date) {
+        this.date = date;
+        this.#csapat1goalnum = randomResultNumber(0, 6);
+        this.#csapat2goalnum = randomResultNumber(0, 6);
+        if (this.#csapat1goalnum > this.#csapat2goalnum) {
+            this.#eredmeny = this.#csapat1goalnum+"-"+this.#csapat2goalnum
+            this.winner = csapat1;
+            this.#loser = csapat2;
+        }else{
+            this.#eredmeny = this.#csapat2goalnum+"-"+this.#csapat1goalnum
+            this.winner = csapat2;
+            this.#loser = csapat1;
+        }
+    }
+    result(): string {
+        return "Football match: "+this.winner+" - "+this.#loser+" ("+this.#eredmeny+")";
+    }
+}
