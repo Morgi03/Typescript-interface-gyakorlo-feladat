24989739d4945413c9f169ba83db348607f20aaa
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-19 14:29:17 +0200

    Marathon class elkészítése

--- src/index.js
+++ src/index.js
@@ -10,34 +10,44 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _Football_loser, _Football_eredmeny, _Football_csapat1goalnum, _Football_csapat2goalnum;
+var _Football_loser, _Football_matchresult, _Football_csapat1goalnum, _Football_csapat2goalnum, _Marathon_runtime;
 function randomResultNumber(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 class Football {
     constructor(csapat1, csapat2, date) {
         _Football_loser.set(this, void 0);
-        _Football_eredmeny.set(this, void 0);
+        _Football_matchresult.set(this, void 0);
         _Football_csapat1goalnum.set(this, void 0);
         _Football_csapat2goalnum.set(this, void 0);
         this.date = date;
         __classPrivateFieldSet(this, _Football_csapat1goalnum, randomResultNumber(0, 6), "f");
         __classPrivateFieldSet(this, _Football_csapat2goalnum, randomResultNumber(0, 6), "f");
         if (__classPrivateFieldGet(this, _Football_csapat1goalnum, "f") > __classPrivateFieldGet(this, _Football_csapat2goalnum, "f")) {
-            __classPrivateFieldSet(this, _Football_eredmeny, __classPrivateFieldGet(this, _Football_csapat1goalnum, "f") + "-" + __classPrivateFieldGet(this, _Football_csapat2goalnum, "f"), "f");
+            __classPrivateFieldSet(this, _Football_matchresult, __classPrivateFieldGet(this, _Football_csapat1goalnum, "f") + "-" + __classPrivateFieldGet(this, _Football_csapat2goalnum, "f"), "f");
             this.winner = csapat1;
             __classPrivateFieldSet(this, _Football_loser, csapat2, "f");
         }
         else {
-            __classPrivateFieldSet(this, _Football_eredmeny, __classPrivateFieldGet(this, _Football_csapat2goalnum, "f") + "-" + __classPrivateFieldGet(this, _Football_csapat1goalnum, "f"), "f");
+            __classPrivateFieldSet(this, _Football_matchresult, __classPrivateFieldGet(this, _Football_csapat2goalnum, "f") + "-" + __classPrivateFieldGet(this, _Football_csapat1goalnum, "f"), "f");
             this.winner = csapat2;
             __classPrivateFieldSet(this, _Football_loser, csapat1, "f");
         }
     }
     result() {
-        return "Football match: " + this.winner + " - " + __classPrivateFieldGet(this, _Football_loser, "f") + " (" + __classPrivateFieldGet(this, _Football_eredmeny, "f") + ")";
+        return "Football match: " + this.winner + " - " + __classPrivateFieldGet(this, _Football_loser, "f") + " (" + __classPrivateFieldGet(this, _Football_matchresult, "f") + ")";
     }
 }
-_Football_loser = new WeakMap(), _Football_eredmeny = new WeakMap(), _Football_csapat1goalnum = new WeakMap(), _Football_csapat2goalnum = new WeakMap();
+_Football_loser = new WeakMap(), _Football_matchresult = new WeakMap(), _Football_csapat1goalnum = new WeakMap(), _Football_csapat2goalnum = new WeakMap();
 class Marathon {
+    constructor(winner, date, runtimeinsec) {
+        _Marathon_runtime.set(this, void 0);
+        this.winner = winner;
+        this.date = date;
+        __classPrivateFieldSet(this, _Marathon_runtime, runtimeinsec, "f");
+    }
+    result() {
+        return "Marathon: " + __classPrivateFieldGet(this, _Marathon_runtime, "f") / 60 + " min " + __classPrivateFieldGet(this, _Marathon_runtime, "f") % 60 + " s";
+    }
 }
+_Marathon_runtime = new WeakMap();
--- src/index.ts
+++ src/index.ts
@@ -12,7 +12,7 @@ class Football implements Results {
     winner: string;
     date: Date;
     #loser: string;
-    #eredmeny: string;
+    #matchresult: string;
     #csapat1goalnum: number;
     #csapat2goalnum: number;
 
@@ -21,16 +21,33 @@ class Football implements Results {
         this.#csapat1goalnum = randomResultNumber(0, 6);
         this.#csapat2goalnum = randomResultNumber(0, 6);
         if (this.#csapat1goalnum > this.#csapat2goalnum) {
-            this.#eredmeny = this.#csapat1goalnum+"-"+this.#csapat2goalnum
+            this.#matchresult = this.#csapat1goalnum + "-" + this.#csapat2goalnum
             this.winner = csapat1;
             this.#loser = csapat2;
-        }else{
-            this.#eredmeny = this.#csapat2goalnum+"-"+this.#csapat1goalnum
+        } else {
+            this.#matchresult = this.#csapat2goalnum + "-" + this.#csapat1goalnum
             this.winner = csapat2;
             this.#loser = csapat1;
         }
     }
     result(): string {
-        return "Football match: "+this.winner+" - "+this.#loser+" ("+this.#eredmeny+")";
+        return "Football match: " + this.winner + " - " + this.#loser + " (" + this.#matchresult + ")";
     }
 }
+
+
+class Marathon implements Results {
+    winner: string;
+    date: Date;
+    #runtime : number;
+    constructor(winner: string, date: Date, runtimeinsec: number) {
+        this.winner = winner;
+        this.date = date;
+        this.#runtime = runtimeinsec;
+    }
+
+    result(): string {
+        return "Marathon: "+ this.#runtime/60+" min "+this.#runtime%60+" s";
+    }
+}
+
